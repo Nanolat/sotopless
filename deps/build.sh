16fc90361f5036507d8f261a15175c5cdd8e0c43
Kangmo Kim <kangmo@nanolat.com>
2013-08-19 15:24:50 -0700

    Added Nanolat Database in deps/build.sh

--- deps/build.sh
+++ deps/build.sh
@@ -2,12 +2,31 @@
 # archives - contains tarballs or zip files of open source projects on that Nanolat Database depends.
 # lib - contains libraries built.
 # include - contains header files of libraries.
+
+echo "Build started at `date`"
+
 EXT_LIB_HOME=`pwd`
 
 rm -rf include lib bin build
 mkdir include lib bin build
 
 cd build
+
+#########################################################
+# build nldb
+#########################################################
+pushd .
+git clone https://github.com/Nanolat/nldb.git
+cd nldb
+# Checkout with the commit hash representing the version of nldb compatible with SoTopless.
+# Commit : Fixed makefile of gtest to solve the build break issue.
+git checkout d1210cabcf82f76f39af555cf5588d42d3aea258
+./build.sh
+
+cp -r ./src/include/nldb ${EXT_LIB_HOME}/include
+cp ./src/nldb/libnldb.so ${EXT_LIB_HOME}/lib
+
+popd
 
 #########################################################
 # build gtest
@@ -22,14 +41,15 @@ cp -r include/* ${EXT_LIB_HOME}/include
 cp lib/.libs/* ${EXT_LIB_HOME}/lib
 popd
 
-
-
+#########################################################
+# build xUnit++ 
+#########################################################
 pushd .
 tar xvfz ${EXT_LIB_HOME}/archives/xunit-1.3.2.tar.gz
 
 cd xunit-1.3.2/xUnit++    
 
-cp -r ../xUnit++ ${EXT_LIB_HOME}/include
+cp -r ../xUnit++/xUnit++ ${EXT_LIB_HOME}/include
 
 cd src
 make
@@ -43,7 +63,9 @@ cp ./xUnit ${EXT_LIB_HOME}/bin
 
 popd
 
-
+#########################################################
+# build boost
+#########################################################
 
 pushd .
 # Boost is too big > 50MB to put into our repository. Download it from internet.
@@ -61,4 +83,4 @@ mv ${EXT_LIB_HOME}/boost_install/lib/* ${EXT_LIB_HOME}/lib
 rm -rf ${EXT_LIB_HOME}/boost_install
 popd
 
-
+echo "Build ended at `date`"
